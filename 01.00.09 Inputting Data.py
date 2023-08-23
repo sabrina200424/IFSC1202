93e4443944096c3951a730ece0c34db58b75f7bd
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2023-08-23 15:59:16 +0000

    Added Python Content

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("First Name: ")  # Input statement without a prompt
+print("You Entered: " + firstname)
+
+lastname = input("Enter Your Name:")  # Input statement with a prompt
+print("You Entered: " + lastname)
---
+++ 01.01 Sum of Three Numbers.py
@@ -0,0 +1,6 @@
+x = input("First Number: ")
+y = input("Second Number: ")
+z = input("Third Number: ")
+sum = int(x)+int(y)+int(z)
+
+print(sum)
