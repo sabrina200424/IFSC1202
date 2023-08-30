91818d2f53e2a76925208eac6a21a587ba395c44
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2023-08-30 02:30:49 +0000

    Added comments for understanding

--- 01.Project.py
+++ 01.Project.py
@@ -1,5 +1,6 @@
 amount = int(input("Enter length of time in days: "))
-x = amount // 365
+x = amount // 365  
+#add in floor divison
 Years = "Years: "
 print(Years , x)
 amount = amount - (x * 365)
@@ -8,5 +9,6 @@ Weeks = "Weeks: "
 print(Weeks, x)
 y = x * 7
 z = amount - y
+#put in equation before print
 Days = "Days: "
 print("Days:", z)
