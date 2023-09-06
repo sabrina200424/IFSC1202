87b0cf078716a1cdf0d835aeda55a42801608558
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2023-09-06 00:48:47 +0000

    Finished project 2

--- 02.Project.py
+++ 02.Project.py
@@ -5,18 +5,26 @@ c = input("Enter side 3: ")
 
 s = (float(a) + float(b) + float(c)) / 2
 
-d = float(s) * float(a)
-e = float(s) * float(b)
-f = float(s) * float(c)
-g = s * s
-h = float(g) - float(d)
-i = float(g) - float(e)
-j = float(g) - float(f)
+#d = float(s) * float(a)
+#e = float(s) * float(b)
+#f = float(s) * float(c)
+#g = s * s
+#h = float(g) - float(d)
+#i = float(g) - float(e)
+#j = float(g) - float(f)
 
-k = float(h) + float(i) + float(j)
+#k = float(h) + float(i) + float(j)
+
+#formula above ended up being wrong, but I'm just keeping code for reference
+
+d = float(s) - float(a)
+e = float(s) - float(b)
+f = float(s) - float(c)
+
+g = float(s) * float(d) * float(e) * float(f)
 
 import math
-area = math.sqrt(k)
+area = math.sqrt(g)
 
 Area = "Area :"
 print(Area, area)
