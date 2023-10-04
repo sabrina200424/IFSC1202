c515420681a30437762b13cd2281045b17668753
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2023-10-04 14:10:51 +0000

    Added comments to project 6

--- 06.Project.py
+++ 06.Project.py
@@ -1,37 +1,35 @@
-# w means to copy the code from a file
+# w means to write a code from a file
+# r means to read the code
  
-inputfilename = "06.Project Input File.txt"
-outputfilename = "06.Project Output File.txt"
-inputrecordcount =0
-mergefilerecordcount =0
-
-inputfile = open(inputfilename,'r')
-
-outputfile = open(outputfilename,'w')
-
-line =inputfile.readline()
-
-while line != '':
-    
-    if line == "**Insert File Here**\n":
-       mergefilename = "06.Project Merge File.txt"
-       mergefile = open(mergefilename, "r")
-       mergeline=mergefile.readline()
-       while mergeline != "":
-           mergefilerecordcount= mergefilerecordcount+1
-           outputfile.write(mergeline)
-           mergeline = mergefile.readline()
-           mergefile.close()
-           outputfile.write("\n")
-           line=inputfile.readline()
+projectinputfile = open("06.Project Input File.txt", "r")
+projectoutputfile = open("06.Project Output File.txt", "w")
+# set the record to 0 before adding in different numbers 
+inputrecord = 0
+outputrecord = 0
+mergerecord = 0
+line = projectinputfile.readline()
+while line != "":
+    if line == "**Insert Merge File Here**\n":
+        # \n means make a new line
+        # starts a new line after the insert merge file comment 
+        projectmergefile = open("06.Project Merge File.txt","r")
+        # open the file first to start adding code in
+        mergeline = projectmergefile.readline()
+        while mergeline != "":
+            projectoutputfile.write(mergeline)
+            outputrecord += 1
+            mergeline = projectmergefile.readline()
+            mergerecord += 1
+        projectoutputfile.write("\n")
+        projectmergefile.close()
     else:
-        inputrecordcount = inputrecordcount+1
-        outputfile.write(line)
-        line=inputfile.readline()
-        
-inputfile.close()
-outputfile.close()
-
-print("{} input file records".format(inputrecordcount))
-print("{} merge file records".format(mergefilerecordcount))
-print("{} output file records".format(inputrecordcount+mergefilerecordcount))
+        projectoutputfile.write(line)
+        outputrecord += 1
+        inputrecord += 1
+    line = projectinputfile.readline()
+projectinputfile.close()
+projectoutputfile.close()
+print(inputrecord,"input file record")
+print(mergerecord, "merge file record")
+print(outputrecord, "output file record")
+# output record changes depending on your code
